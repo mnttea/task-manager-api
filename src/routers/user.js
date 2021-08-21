0a0cbf80a08eb1cb630d119f8dfc40b777284a24
mntteass <mntteass@gmail.com>
2021-08-20 17:51:12 -0700

    creating user also automatically logs in user

--- src/routers/user.js
+++ src/routers/user.js
@@ -10,9 +10,9 @@ router.post('/users', async (req, res) => {
 	const user = new User(req.body);
 
 	try {
-		await user.save();
 		sendWelcomeEmail(user.email, user.name);
 		const token = await user.generateAuthToken();
+		await user.save();
 		res.status(201).send({ user, token });
 	} catch (error) {
 		res.status(400).send(error);
